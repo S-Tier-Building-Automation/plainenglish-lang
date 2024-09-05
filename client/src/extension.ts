45ef6638f5bfaeebb30822af0123df9060b7f430
Timothy Fedorko <tfedorko@JSCLPTP-6-10-24.JSCBMS.local>
2024-09-05 11:52:30 -0400

    Add auto-update feature using GitHub

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,5 +1,7 @@
 import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
+import { workspace, ExtensionContext, window, commands, Uri } from 'vscode';
+import * as https from 'https';
+import * as fs from 'fs';
 
 import {
   LanguageClient,
@@ -40,6 +42,41 @@ export function activate(context: ExtensionContext) {
   );
 
   client.start();
+
+  // Check for updates every day
+  setInterval(checkForUpdates, 24 * 60 * 60 * 1000);
+  // Also check for updates on activation
+  checkForUpdates();
+}
+
+function checkForUpdates() {
+  const versionUrl = 'https://raw.githubusercontent.com/S-Tier-Building-Automation/plainenglish-lang/main/version-info.json';
+  https.get(versionUrl, (res) => {
+    let data = '';
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+    res.on('end', () => {
+      const versionInfo = JSON.parse(data);
+      const currentVersion = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'package.json'), 'utf8')).version;
+      if (versionInfo.version !== currentVersion) {
+        promptForUpdate(versionInfo.downloadUrl);
+      }
+    });
+  }).on('error', (err) => {
+    console.error('Error checking for updates:', err);
+  });
+}
+
+function promptForUpdate(downloadUrl: string) {
+  window.showInformationMessage(
+    'A new version of the Plain English Language extension is available. Would you like to download it?',
+    'Yes', 'No'
+  ).then(selection => {
+    if (selection === 'Yes') {
+      commands.executeCommand('vscode.open', Uri.parse(downloadUrl));
+    }
+  });
 }
 
 export function deactivate(): Thenable<void> | undefined {
